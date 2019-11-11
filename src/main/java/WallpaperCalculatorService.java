31e4e12cbb9235c2bf081985bbd14e34b2f09b99
student <student@localhost>
2019-11-11 21:40:44 +0300

    редактирование валидации

--- src/main/java/WallpaperCalculatorService.java
+++ src/main/java/WallpaperCalculatorService.java
@@ -2,26 +2,43 @@ public class WallpaperCalculatorService {
 
 
     public int calculateWithoutAlignment(double widthRoom, double lengthRoom, double heightRoom, double widthWallpaper, double lengthWallpaper) {
-        if (widthRoom <= 0 && lengthRoom <= 0 && heightRoom <= 0 && widthWallpaper <= 0 && lengthWallpaper <= 0) {
+        if (widthRoom <= 0) {
             return 0;
-        } else {
+        } else if (lengthRoom <= 0) {
+            return 0;
+        } else if (heightRoom <= 0) {
+            return 0;
+        } else if (widthWallpaper <= 0) {
+            return 0;
+        } else if (lengthWallpaper <= 0) {
+            return 0;
+        }
             double perimeterRoom = widthRoom * lengthRoom * 2;
             double calculationOfPanels = Math.ceil(perimeterRoom / widthWallpaper);//31
             double numberOfRolls = Math.ceil(calculationOfPanels / (lengthWallpaper / (heightRoom + 0.10)));//   31/10/3.1
             int intNumberOfRolls = (int) numberOfRolls;
             return intNumberOfRolls;
-        }
     }
 
     public int calculateWithDisplacement(double widthRoom, double lengthRoom, double heightRoom, double widthWallpaper, double lengthWallpaper, double offsetLength) {
-        if (widthRoom <= 0 & lengthRoom <= 0 & heightRoom <= 0 & widthWallpaper <= 0 & lengthWallpaper <= 0 & offsetLength <= 0) {
+        if (widthRoom <= 0) {
             return 0;
-        } else {
+        } else if (lengthRoom <= 0) {
+            return 0;
+        } else if (heightRoom <= 0) {
+            return 0;
+        } else if (widthWallpaper <= 0) {
+            return 0;
+        } else if (lengthWallpaper <= 0) {
+            return 0;
+        } else if (offsetLength <= 0) {
+            return 0;
+        }
             double perimeterRoom = (widthRoom * lengthRoom) * 2;
             double calculationOfPanels = Math.ceil(perimeterRoom / widthWallpaper);
             double numberOfRolls = Math.ceil(calculationOfPanels / (lengthWallpaper / (heightRoom + 0.10 + offsetLength)));
             int intNumberOfRolls = (int) numberOfRolls;
             return intNumberOfRolls;
-        }
+
     }
 }
--- src/test/java/WallpaperCalculatorServiceTest.java
+++ src/test/java/WallpaperCalculatorServiceTest.java
@@ -17,15 +17,69 @@ class WallpaperCalculatorServiceTest {
     }
 
     @Test
-    public void calculateWithDisplacementTestAttNull() {
+    public void calculateWithDisplacementTestAttNullwidthRoom() {
         WallpaperCalculatorService wallpaperCalculator = new WallpaperCalculatorService();
         assertEquals(0, wallpaperCalculator.calculateWithDisplacement(-10, -10, -200, -300, -1, -10));
     }
 
     @Test
-    public void calculateWithoutAlignmentTestAttNull() {
+    public void calculateWithDisplacementTestAttNulllengthRoom() {
+        WallpaperCalculatorService wallpaperCalculator = new WallpaperCalculatorService();
+        assertEquals(0, wallpaperCalculator.calculateWithDisplacement(1, -300, -200, -300, -1, -10));
+    }
+
+    @Test
+    public void calculateWithDisplacementTestAttNullheightRoom() {
+        WallpaperCalculatorService wallpaperCalculator = new WallpaperCalculatorService();
+        assertEquals(0, wallpaperCalculator.calculateWithDisplacement(1, 1, -200, -300, -1, -10));
+    }
+
+    @Test
+    public void calculateWithDisplacementTestAttNullwidthWallpaper() {
+        WallpaperCalculatorService wallpaperCalculator = new WallpaperCalculatorService();
+        assertEquals(0, wallpaperCalculator.calculateWithDisplacement(1, 1, 1, -300, -1, -10));
+    }
+
+    @Test
+    public void calculateWithDisplacementTestAttNulllengthWallpaper() {
+        WallpaperCalculatorService wallpaperCalculator = new WallpaperCalculatorService();
+        assertEquals(0, wallpaperCalculator.calculateWithDisplacement(1, 1, 1, 1, -1, -10));
+    }
+
+    @Test
+    public void calculateWithDisplacementTestAttNulloffsetLength() {
+        WallpaperCalculatorService wallpaperCalculator = new WallpaperCalculatorService();
+        assertEquals(0, wallpaperCalculator.calculateWithDisplacement(1, 1, 1, 1, 1, -10));
+    }
+
+    @Test
+    public void calculateWithoutAlignmentTestAttNullwidthRoom() {
         WallpaperCalculatorService wallpaperCalculator = new WallpaperCalculatorService();
         assertEquals(0, wallpaperCalculator.calculateWithoutAlignment(-10, -20, -20, -30, -5));
+    }
+
+    @Test
+    public void calculateWithoutAlignmentTestAttNulllengthRoom() {
+        WallpaperCalculatorService wallpaperCalculator = new WallpaperCalculatorService();
+        assertEquals(0, wallpaperCalculator.calculateWithoutAlignment(1, -20, -20, -30, -5));
+    }
+
+    @Test
+    public void calculateWithoutAlignmentTestAttNullheightRoom() {
+        WallpaperCalculatorService wallpaperCalculator = new WallpaperCalculatorService();
+        assertEquals(0, wallpaperCalculator.calculateWithoutAlignment(1, 1, -20, -30, -5));
+    }
+
+    @Test
+    public void calculateWithoutAlignmentTestAttNullwidthWallpaper() {
+        WallpaperCalculatorService wallpaperCalculator = new WallpaperCalculatorService();
+        assertEquals(0, wallpaperCalculator.calculateWithoutAlignment(1, 1, 1, -30, -5));
+    }
+
+    @Test
+    public void calculateWithoutAlignmentTestAttNulllengthWallpaper() {
+        WallpaperCalculatorService wallpaperCalculator = new WallpaperCalculatorService();
+        assertEquals(0, wallpaperCalculator.calculateWithoutAlignment(1, 1, 1, 1, -5));
     }
 
 }
