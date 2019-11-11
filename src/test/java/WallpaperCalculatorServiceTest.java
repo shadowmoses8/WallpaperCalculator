cc0b3e9ccad478348a7045373b6b5042ffe83879
student <student@localhost>
2019-11-11 12:38:12 +0300

    изменение теста для улучшения покрытия ver2

--- src/test/java/WallpaperCalculatorServiceTest.java
+++ src/test/java/WallpaperCalculatorServiceTest.java
@@ -25,7 +25,7 @@ class WallpaperCalculatorServiceTest {
     @Test
     public void calculateWithoutAlignmentTestAttNull() {
         WallpaperCalculatorService wallpaperCalculator = new WallpaperCalculatorService();
-        assertEquals(0, wallpaperCalculator.calculateWithoutAlignment(-10, -20, 0, 0, 0));
+        assertEquals(0, wallpaperCalculator.calculateWithoutAlignment(-10, -20, -20, -30, -5));
     }
 
 }
